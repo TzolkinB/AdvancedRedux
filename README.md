093f6e4e6b6a0da8359aa912d72b384e84554194
Kimberly Bell <kimberly.bell@tma1.com>
2018-05-11 11:26:20 -0400

    updated npm packages and changes webpack.config file

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Advanced Redux - Udemy Tutorial
- -- Stephen Grider
-[Advanced Redux](https://www.udemy.com/react-redux-tutorial/)
+# Advanced Redux
+## Udemy Tutorial: Advanced Redux by Stephen Grider
+- [Advanced Redux on Udemy](https://www.udemy.com/react-redux-tutorial/)
+- his repo for the course: https://github.com/StephenGrider/AdvancedReduxCode
 
 # ReduxSimpleStarter
-clone ReduxSimpleStarter boilerplate template from:
- git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
+cloned ReduxSimpleStarter boilerplate template from:
+https://github.com/StephenGrider/ReduxSimpleStarter.git
 
 
 ### Getting Started
@@ -13,3 +14,9 @@ clone ReduxSimpleStarter boilerplate template from:
 > npm install
 > npm start
 ```
+
+- had to update the boilerplate from react-router 2 to 3.2.0
+- NPM packages used:
+    - Mocha
+    - Chai
+    - chai-jquery
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
   entry: ['./src/index.js'],
   output: {
-    path: __dirname,
+    path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpg|gif|eot|woff|woff2|ttf)$/,
+        use: [{ loader: 'file-loader?name=[name].[ext]' }],
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }]
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
+    port: 8080,
+    compress: true,
+    stats: 'errors-only',
     watchOptions: {
       aggregateTimeout: 300,
       poll: 1000
