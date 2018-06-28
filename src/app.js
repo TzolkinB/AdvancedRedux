683a5a69ae66529f5ccc45bb87483cee9f4c1dc0
Kimberly Bell <kimberly.bell@tma1.com>
2018-06-28 12:45:10 -0400

    initial firebase set up

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React        from 'react';
 import { render }   from 'react-dom';
 import { Provider } from 'react-redux';
+import firebase     from './firebase';
 import {
   BrowserRouter, Route, Switch,
   Redirect
